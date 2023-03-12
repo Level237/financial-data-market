78a6cb55eed4e6325710a4f3d829f842b88b5b21
Balep Martin <84333489+Level237@users.noreply.github.com>
2023-03-12 17:47:36 +0000

    search with params

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,3 +1,42 @@
+import {useState,useEffect } from "react";
+import finnHub from "../apis/finnHub"
 export const AutoComplete=()=>{
-  return <div>AutoComplete</div>
+
+  const [search,setSearch]=useState("")
+  const [results,setResults]=useState([])
+
+  useEffect(()=>{
+    let isMounted=true
+    const fetchData=async() =>{
+      try {
+        const response= await finnHub.get("/search",{
+          params:{
+            q:search
+          }
+        })
+        if(isMounted){
+          setResult(response.data)
+        }
+        console.log(response)
+        
+      } catch (error) {
+        
+      }
+    }
+    if(search.length > 0){
+      fetchData();
+    }
+    return ()=> (isMounted = false)
+  },[search])
+  return <div className="w-50 p-5 rounded mx-auto">
+  <div className="form-floating dropdown">
+    <input style={{backgroundColor:"rgba(145,158,171,0.04)"}} id="search" type="text" className="form-control" placeholder="Search" autoComplete="off" value={search} onChange={(e)=>setSearch(e.target.value)}></input>
+    <label htmlFor="search">Search</label>
+    <ul className="dropdown-menu">
+      <li>Stock1</li>
+      <li>Stock2</li>
+      <li>Stock3</li>
+    </ul>
+  </div>
+  </div>
 }
--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,38 +1,46 @@
 import { useState,useEffect } from "react";
+import {BsFillCaretDownFill,BsFillCaretUpFill} from "react-icons/bs"
 import finnHub from "../apis/finnHub";
 
 export const StockList=()=>{
 
-  const  [stock,setStock]=useState();
+  const  [stock,setStock]=useState([]);
  const [watchList,setWatchList]= useState(['GOOGL',"MSFT","AMZN"]);
 
+  const changeColor=(change)=>{
+    return change > 0 ? 'success' : "danger";
+  }
+
+  const renderIcon=(change)=>{
+    return change > 0 ? <BsFillCaretUpFill/> : <BsFillCaretDownFill/>
+  }
   useEffect(()=>{
      let isMounted=true;
     const fetchData=async ()=>{
-      const response=[]
+      
+      const responses=[]
       try{
-      const response1=await  finnHub.get("/quote",{
-        params:{
-          symbol:"GOOGL"
-        }
-      })
-        responses.push(response1);
-         const response2=await  finnHub.get("/quote",{
-        params:{
-          symbol:"MSFT"
-        }
-      })
-        responses.push(response2);
-         const response3=await  finnHub.get("/quote",{
-        params:{
-          symbol:"AMZN"
-        }
-      })
-       responses.push(response3);
+
+
+       const responses= await Promise.all(watchList.map((stock)=>{
+         return finnHub.get("/quote",{
+           params: {
+             symbol:stock
+           }
+         })
+       }))
+       
         console.log(responses);
-        console.log("le");
+          const data=responses.map((response)=>{
+            return {
+                 data : response.data,
+            symbol: response.config.params.symbol
+            }
+           
+          })
+        console.log(data);
         if(isMounted){
-            setStock(response.data)
+            setStock(data)
         }
         
       }catch(err){
@@ -43,5 +51,36 @@ export const StockList=()=>{
 
     return ()=>(isMounted=false)
   }, []) 
-  return <div>StockList</div>
+  
+  return <table className="table hover mt-5">
+  <thead style={{color:"rgb(79,89,102)"}}>
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Last</th>
+      <th scope="col">Chg</th>
+      <th scope="col">Chg%</th>
+      <th scope="col">Hight</th>
+      <th scope="col">Low</th>
+      <th scope="col">Open</th>
+      <th scope="col">Pclose</th>
+    </tr>
+  </thead>
+    <tbody>
+      {stock.map((stockData)=>{
+    return (
+      <tr className="table-row" key={stockData.symbol}>
+            <th scope="row">{stockData.symbol}</th>
+          <td>{stockData.data.c}</td>
+          <td className={`text-${changeColor(stockData.data.d)}`}>{stockData.data.d} {renderIcon(stockData.data.d)}</td>
+          <td className={`text-${changeColor(stockData.data.dp)}`}>{stockData.data.dp} {renderIcon(stockData.data.d)}</td>
+          <td>{stockData.data.h}</td>
+          <td>{stockData.data.l}</td>
+          <td>{stockData.data.o}</td>
+          <td>{stockData.data.pc}</td>
+      </tr>
+    )
+      })}
+      
+    </tbody>
+  </table>
 }
