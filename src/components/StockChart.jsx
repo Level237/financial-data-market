4dc49678bebdb3f6feb9f7559e65cf4caa765271
Balep Martin <84333489+Level237@users.noreply.github.com>
2023-03-18 09:06:51 +0000

    stats with apexChart js

--- src/components/StockChart.jsx
+++ src/components/StockChart.jsx
@@ -1,4 +1,30 @@
-export const StockChart=({chartData})=>{
+import Chart from "react-apexcharts"
+
+export const StockChart=({chartData,symbol})=>{
   const {day,week,year}=chartData
-  return <h3>StockChart</h3>
+  const options={
+    title:{
+      text:symbol,
+      align:"center",
+      style:{
+        fontSize:"24px"
+      }
+    },
+    chart:{
+      id:"stock data",
+      animations:{
+        speed:1300
+      }
+    },
+    xaxis:{
+      type:"datetime"
+    }
+  }
+  const series=[{
+    name:symbol,
+    data:day
+  }]
+  return <div  className="mt-5 p-4 shadow-sm bg-white">
+    <Chart options={options} series={series} type="area" width="100%" />
+  </div>
 }
